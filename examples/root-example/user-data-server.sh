fc7d9a87330ff8ac041370faef3dab1ce7c82382
Kevin Stuart <kevin.stuart@venafi.com>
2018-09-13 13:30:55 -0600

    Added host variables and updated supervisor to load the zookeeper config.

--- examples/root-example/user-data-server.sh
+++ examples/root-example/user-data-server.sh
@@ -6,8 +6,13 @@ cat << EOF > /usr/local/zookeeper/conf/zoo.cfg
 tickTime=2000
 dataDir=/var/lib/zookeeper
 clientPort=2181
+initLimit=5
+syncLimit=2
+server.1=${zookeeper_01}:2888:3888
+server.2=${zookeeper_02}:2888:3888
+server.3=${zookeeper_03}:2888:3888
 EOF
 
 # Start Zookeeper
-/usr/local/zookeeper/bin/zkServer.sh start
+supervisorctl reload
 
