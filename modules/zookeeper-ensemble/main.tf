e10a5da031b4d209fd3d3573b89ed709b0f0f55a
Kevin Stuart <kevin.stuart@venafi.com>
2018-09-27 16:00:03 -0600

    It works!

--- modules/zookeeper-ensemble/main.tf
+++ modules/zookeeper-ensemble/main.tf
@@ -216,7 +216,9 @@ module "zookeeper_config" {
   zookeeper_config_bucket = "${var.zookeeper_config_bucket}"
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
 # Testing Lambda function to update dns on asg event.
+# ---------------------------------------------------------------------------------------------------------------------
 
 # Create sns topic
 resource "aws_sns_topic" "zookeeper_asg_updates" {
@@ -273,26 +275,37 @@ data "aws_iam_policy_document" "lambda_asg_route53" {
     effect = "Allow"
 
     actions = [
-      "ec2:Describe*",
-      "elasticloadbalancing:Describe*",
-      "cloudwatch:ListMetrics",
-      "cloudwatch:GetMetricStatistics",
-      "cloudwatch:Describe*",
       "autoscaling:Describe*",
+      "cloudfront:ListDistributions",
+      "cloudwatch:Describe*",
+      "cloudwatch:DescribeAlarms",
+      "cloudwatch:GetMetricStatistics",
+      "cloudwatch:GetMetricStatistics",
+      "cloudwatch:ListMetrics",
+      "ec2:CreateNetworkInterface",
+      "ec2:DeleteNetworkInterface",
+      "ec2:Describe*",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeRegions",
+      "ec2:DescribeVpcs",
+      "elasticbeanstalk:DescribeEnvironments",
+      "elasticloadbalancing:Describe*",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+      "logs:FilterLogEvents",
+      "logs:GetLogEvents",
+      "logs:PutLogEvents",
       "route53:*",
       "route53domains:*",
-      "cloudfront:ListDistributions",
-      "elasticloadbalancing:DescribeLoadBalancers",
-      "elasticbeanstalk:DescribeEnvironments",
-      "s3:ListBucket",
       "s3:GetBucketLocation",
       "s3:GetBucketWebsite",
-      "ec2:DescribeVpcs",
-      "ec2:DescribeRegions",
-      "sns:ListTopics",
+      "s3:ListBucket",
       "sns:ListSubscriptionsByTopic",
-      "cloudwatch:DescribeAlarms",
-      "cloudwatch:GetMetricStatistics"
+      "sns:ListTopics",
     ]
 
     resources = ["*"]
@@ -300,25 +313,51 @@ data "aws_iam_policy_document" "lambda_asg_route53" {
 }
 
 # Create Lambda function to process ASG events.
-resource "aws_lambda_function" "zookeeper_asg_dns_lambda" {
-  #filename         = "${file("${path.module}/asg_dns_updater.zip")}"
-  filename         = "asg_dns_updater.zip"
-  function_name    = "asg_dns_updater"
-  role             = "${aws_iam_role.lambda_role.arn}"
-  handler          = "exports.handler"
-  #source_code_hash = "${base64sha256(file("asg_dns_updater.zip"))}"
-  runtime          = "nodejs8.10"
+variable "asg_lambda_file" {
+  description = "File to use for the ASG Lambda function"
+  default     = "./examples/root-example/asg_dns_updater.zip"
+}
 
-  environment {
-    variables = {
-      foo = "bar"
-    }
+resource "aws_lambda_function" "zookeeper_asg_dns_lambda" {
+  function_name = "asg_dns_updater"
+
+  filename         = "${var.asg_lambda_file}"
+  handler          = "asg_dns_updater.handler"
+  role             = "${aws_iam_role.lambda_role.arn}"
+  source_code_hash = "${base64sha256(file("${var.asg_lambda_file}"))}"
+
+  runtime     = "nodejs8.10"
+  memory_size = 128
+  timeout     = 10
+
+  vpc_config {
+    subnet_ids         = ["${var.subnet_ids}"]
+    security_group_ids = ["${aws_security_group.lc_security_group.id}"]
   }
 }
 
-resource "aws_lambda_event_source_mapping" "event_source_mapping" {
-  batch_size        = 1
-  event_source_arn  = "${aws_sns_topic.zookeeper_asg_updates.arn}"
-  enabled           = true
-  function_name     = "${aws_lambda_function.zookeeper_asg_dns_lambda.arn}"
+resource "aws_lambda_alias" "asg_event" {
+    name                 = "asg_event"
+    description          = "Autoscaling event"
+
+    function_name        = "${aws_lambda_function.zookeeper_asg_dns_lambda.arn}"
+    function_version     = "$LATEST"
+}
+
+resource "aws_lambda_permission" "asg_event" {
+    function_name        = "${aws_lambda_function.zookeeper_asg_dns_lambda.arn}"
+
+    statement_id         = "AllowExecutionFromSNS"
+    action               = "lambda:InvokeFunction"
+    principal            = "sns.amazonaws.com"
+
+    source_arn           = "${aws_sns_topic.zookeeper_asg_updates.arn}"
+}
+
+resource "aws_sns_topic_subscription" "asg_event" {
+    topic_arn            = "${aws_sns_topic.zookeeper_asg_updates.arn}"
+
+    protocol             = "lambda"
+
+    endpoint             = "${aws_lambda_function.zookeeper_asg_dns_lambda.arn}"
 }
