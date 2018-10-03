f2d38bea556a44cacf703a90d20141b537520583
Kevin Stuart <kevin.stuart@venafi.com>
2018-10-02 19:04:07 -0600

    Added java.env file parameters per Solr guide for garbage collection.

--- examples/root-example/user-data-exhibitor.sh
+++ examples/root-example/user-data-exhibitor.sh
@@ -6,12 +6,11 @@ mkdir -p /etc/exhibitor
 cat << EOF > /etc/exhibitor/exhibitor.properties
 auto-manage-instances-apply-all-at-once=0
 auto-manage-instances=1
-java-environment=-Xms2048m -Xmx2048m -verbose\:gc -XX\:+PrintHeapAtGC -XX\:+PrintGCDetails -XX\:+PrintGCDateStamps -XX\:+PrintGCTimeStamps -XX\:+PrintTenuringDistribution -XX\:+PrintGCApplicationStoppedTime -Xloggc\:$ZOO_LOG_DIR/zookeeper_gc.log -XX\:+UseGCLogFileRotation -XX\:NumberOfGCLogFiles\=9 -XX\:GCLogFileSize\=20M
+java-environment=SERVER_JVMFLAGS="-Xms2048m -Xmx2048m -verbose:gc -XX:+PrintHeapAtGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime -Xloggc:$ZOO_LOG_DIR/zookeeper_gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=9 -XX:GCLogFileSize=20M"
 log4j-properties=INFO,ROLLINGFILE
 zoo-cfg-extra=syncLimit\=2&tickTime\=2000&initLimit\=5&autopurge.snapRetainCount\=3&autopurge.purgeInterval\=1
 zookeeper-data-directory=/var/lib/zookeeper
 zookeeper-install-directory=/usr/local/zookeeper
-zookeeper-log-directory=/var/log/zookeeper
 EOF
 
 # Update the Exhibitor Systemd Unit
