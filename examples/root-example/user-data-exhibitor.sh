33492d5d533774d7a3295eea21f78411fd6b62c7
Kevin Stuart <kevin.stuart@venafi.com>
2018-10-03 08:53:38 -0600

    Added jvmflags to set jute max buffer to 50MiB per Solr Ref Guide.

--- examples/root-example/user-data-exhibitor.sh
+++ examples/root-example/user-data-exhibitor.sh
@@ -6,14 +6,14 @@ mkdir -p /etc/exhibitor
 cat << EOF > /etc/exhibitor/exhibitor.properties
 auto-manage-instances-apply-all-at-once=0
 auto-manage-instances=1
-java-environment=ZOO_LOG_DIR="/var/log/zookeeper" SERVER_JVMFLAGS="-Xms2048m -Xmx2048m -verbose:gc -XX:+PrintHeapAtGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime -Xloggc:$ZOO_LOG_DIR/zookeeper_gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=9 -XX:GCLogFileSize=20M"
+java-environment=JVMFLAGS="$JVMFLAGS -Djute.maxbuffer=50000000" ZOO_LOG_DIR="/var/log/zookeeper" SERVER_JVMFLAGS="-Xms2048m -Xmx2048m -verbose:gc -XX:+PrintHeapAtGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime -Xloggc:$ZOO_LOG_DIR/zookeeper_gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=9 -XX:GCLogFileSize=20M"
 log4j-properties=INFO,ROLLINGFILE
 zoo-cfg-extra=syncLimit\=2&tickTime\=2000&initLimit\=5&autopurge.snapRetainCount\=3&autopurge.purgeInterval\=1
 zookeeper-data-directory=/var/lib/zookeeper
 zookeeper-install-directory=/usr/local/zookeeper
 EOF
 
-chown -R zookeeper /etc/exhibitor
+chown -R zookeeper:zookeeper /etc/exhibitor
 
 # Update the Exhibitor Systemd Unit
 cat << EOF > /etc/systemd/system/exhibitor.service
@@ -36,6 +36,7 @@ WantedBy=multi-user.target
 EOF
 
 chmod 664 /etc/systemd/system/exhibitor.service
+chown -R zookeeper /var/log/zookeeper
 
 systemctl daemon-reload
 systemctl restart exhibitor.service
